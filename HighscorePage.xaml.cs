b96f3e541b7cc15c310b39bcd5a5a67042a3b162
WendyDuong <36827768+WendyDuong@users.noreply.github.com>
2020-07-05 17:55:32 +0200

    Add files via upload

---
+++ HighscorePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HighscorePage : ContentPage
+    {
+        public HighscorePage(string score)
+        {
+            InitializeComponent();
+
+            var userScore = this.FindByName<Label>("score");
+            userScore.Text += score;
+
+        }
+        public void BackButton(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
